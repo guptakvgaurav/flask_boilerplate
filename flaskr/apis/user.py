d8835ff90206eece8b9dab1bb89c95a0b35f5c1a
guptakvgaurav <guptakvgaurav@gmail.com>
2018-12-29 15:59:47 +0530

    Added sample apis which consumes data from client and echo them back

--- flaskr/apis/user.py
+++ flaskr/apis/user.py
@@ -1,11 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from flask import current_app as app
 user_blueprint = Blueprint('user', __name__, url_prefix='/user')
 
 
-@user_blueprint.route('/')
-def get():
+@user_blueprint.route('/<int:id>')
+def get(id):
+    # /user/12
     app.logger.info('[User] Start')
-    return "Yo!! user."
+    return jsonify({'id': id})
 
 
+@user_blueprint.route('/search', methods=['GET'])
+def search():
+    # /user/search?title=woodland&min_price=1000
+    app.logger.info('[User] Start')
+    return jsonify(request.args)
+
+
+@user_blueprint.route('/', methods=['POST'])
+def post():
+    return jsonify(request.get_json())
+
+
+@user_blueprint.route('/<int:id>', methods=['PUT'])
+def put(id):
+    param = request.get_json()
+    param.update({'id': id})
+    return jsonify(param)
